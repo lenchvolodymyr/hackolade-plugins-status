4d454701e6590d4c33e58b96f9f6e02e92af989d
Lench Volodymyr <volodymyr.lench@binary-studio.com>
2021-09-22 09:28:01 +0300

    add input with token

--- src/index.js
+++ src/index.js
@@ -1,14 +1,298 @@
 const { default: createRow } = require("./createRow");
+const semver = require('semver');
 
 const fetchPlugins = async () => {
-	const response = await fetch('https://hackolade.com/versionInfo/pluginVersions.json');
+	// const response = await fetch('https://hackolade.com/versionInfo/pluginVersions.json');
 
-	return response.json();
+	// return response.json();
+	return {
+		"plugins": [
+			{
+				"name": "Avro",
+				"target": "Avro",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Apache Avro Schema",
+				"repository": "https://github.com/hackolade/Avro",
+				"version": "0.1.72"
+			},
+			{
+				"name": "BigQuery",
+				"target": "BigQuery",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Google BigQuery Standard SQL",
+				"repository": "https://github.com/hackolade/BigQuery",
+				"version": "0.1.0"
+			},
+			{
+				"name": "Cassandra",
+				"target": "CASSANDRA",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Cassandra DataStax",
+				"repository": "https://github.com/hackolade/Cassandra",
+				"version": "0.1.85"
+			},
+			{
+				"name": "CosmosDB-with-Gremlin-API",
+				"target": "COSMOSDB-GREMLIN",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Azure Cosmos DB with Gremlin API",
+				"repository": "https://github.com/hackolade/CosmosDB-with-Gremlin-API",
+				"version": "0.1.17"
+			},
+			{
+				"name": "CosmosDB-with-Mongo-API",
+				"target": "COSMOSDB-MONGO",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Azure Cosmos DB with MongoDB API",
+				"repository": "https://github.com/hackolade/CosmosDB-with-Mongo-API",
+				"version": "0.1.25"
+			},
+			{
+				"name": "CosmosDB-with-SQL-API",
+				"target": "COSMOSDB-SQL",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Azure Cosmos DB with SQL API",
+				"repository": "https://github.com/hackolade/CosmosDB-with-SQL-API",
+				"version": "0.1.28"
+			},
+			{
+				"name": "DeltaLake",
+				"target": "DeltaLake",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Delta Lake on Databricks, including Azure Databricks, and Databricks on AWS",
+				"repository": "https://github.com/hackolade/DeltaLake",
+				"version": "0.1.17"
+			},
+			{
+				"name": "Elasticsearch",
+				"target": "ELASTICSEARCH",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Elasticsearch",
+				"repository": "https://github.com/hackolade/Elasticsearch",
+				"version": "0.1.26"
+			},
+			{
+				"name": "ElasticsearchV7plus",
+				"target": "ELASTICSEARCHv7plus",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Elasticsearch v7 and above",
+				"repository": "https://github.com/hackolade/ElasticsearchV7plus",
+				"version": "0.1.0"
+			},
+			{
+				"name": "EventBridge",
+				"target": "EventBridge",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for AWS EventBridge Schema Registry",
+				"repository": "https://github.com/hackolade/EventBridge",
+				"version": "0.1.13"
+			},
+			{
+				"name": "Firebase",
+				"target": "FIREBASE",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for RealTime Firebase",
+				"repository": "https://github.com/hackolade/Firebase",
+				"version": "0.1.4"
+			},
+			{
+				"name": "Firestore",
+				"target": "FIRESTORE",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Cloud Firestore",
+				"repository": "https://github.com/hackolade/Firestore",
+				"version": "0.1.4"
+			},
+			{
+				"name": "Glue",
+				"target": "Glue",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for AWS Glue Data Catalog",
+				"repository": "https://github.com/hackolade/glue",
+				"version": "0.1.16"
+			},
+			{
+				"name": "HBase",
+				"target": "HBase",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Apache HBase",
+				"repository": "https://github.com/hackolade/HBase",
+				"version": "0.1.15"
+			},
+			{
+				"name": "Hive",
+				"target": "HIVE",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Apache Hive",
+				"repository": "https://github.com/hackolade/Hive",
+				"version": "0.1.62"
+			},
+			{
+				"name": "JanusGraph",
+				"target": "JanusGraph",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for JanusGraph",
+				"repository": "https://github.com/hackolade/JanusGraph",
+				"version": "0.1.1"
+			},
+			{
+				"name": "Joi",
+				"target": "JOI",
+				"author": "Qwin",
+				"authorRepo": "https://github.com/Qwin",
+				"description": "Hackolade plugin for Joi",
+				"repository": "https://github.com/Qwin/Joi",
+				"version": "0.1.5"
+			},
+			{
+				"name": "MariaDB",
+				"target": "MariaDB",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for MariaDB",
+				"repository": "https://github.com/hackolade/MariaDB",
+				"version": "0.1.8"
+			},
+			{
+				"name": "MarkLogic",
+				"target": "MARKLOGIC",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for MarkLogic",
+				"repository": "https://github.com/hackolade/MarkLogic",
+				"version": "0.1.26"
+			},
+			{
+				"name": "MonStor",
+				"target": "MonStor",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for MonStor",
+				"repository": "https://github.com/hackolade/MonStor",
+				"version": "0.1.11"
+			},
+			{
+				"name": "Neo4j",
+				"target": "NEO4J",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Neo4j",
+				"repository": "https://github.com/hackolade/neo4j",
+				"version": "0.1.31"
+			},
+			{
+				"name": "Neptune-Gremlin",
+				"target": "Neptune-Gremlin",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Amazon Neptune with Gremlin API",
+				"repository": "https://github.com/hackolade/neptune-gremlin",
+				"version": "0.1.2"
+			},
+			{
+				"name": "OpenAPI",
+				"target": "OPENAPI",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Swagger API",
+				"repository": "https://github.com/hackolade/OpenAPI",
+				"version": "0.1.34"
+			},
+			{
+				"name": "Parquet",
+				"target": "PARQUET",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Apache Parquet schema",
+				"repository": "https://github.com/hackolade/Parquet",
+				"version": "0.1.3"
+			},
+			{
+				"name": "Redshift",
+				"target": "Redshift",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Amazon Redshift",
+				"repository": "https://github.com/hackolade/Redshift",
+				"version": "0.1.3"
+			},
+			{
+				"name": "ScyllaDB",
+				"target": "ScyllaDB",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for ScyllaDB",
+				"repository": "https://github.com/hackolade/ScyllaDB",
+				"version": "0.1.29"
+			},
+			{
+				"name": "Snowflake",
+				"target": "Snowflake",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Snowflake",
+				"repository": "https://github.com/hackolade/Snowflake",
+				"version": "0.1.36"
+			},
+			{
+				"name": "SQLServer",
+				"target": "MSSQLServer",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Microsoft SQL Server and Azure SQL Database",
+				"repository": "https://github.com/hackolade/SQLServer",
+				"version": "0.1.29"
+			},
+			{
+				"name": "Swagger",
+				"target": "Swagger",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Swagger API",
+				"repository": "https://github.com/hackolade/Swagger",
+				"version": "0.1.32"
+			},
+			{
+				"name": "Synapse",
+				"target": "Synapse",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for Azure Synapse (SQL Data Warehouse) and Parallel Data Warehouse",
+				"repository": "https://github.com/hackolade/Synapse",
+				"version": "0.1.26"
+			},
+			{
+				"name": "TinkerPop",
+				"target": "TinkerPop",
+				"author": "hackolade",
+				"authorRepo": "https://github.com/hackolade",
+				"description": "Hackolade plugin for TinkerPop",
+				"repository": "https://github.com/hackolade/tinkerpop",
+				"version": "0.1.7"
+			}
+		]
+	}
 };
 
-const makeRequest = async (url) => {
+const makeRequest = async (url, options) => {
 	try {
-		const response = await fetch(url);
+		const response = await fetch(url, options);
 		const result = await response.json();
 
 		return {
@@ -33,11 +317,23 @@ const getMasterVersion = (plugin) => {
 	return makeRequest(`https://raw.githubusercontent.com/${plugin.author}/${plugin.name}/master/package.json`);
 };
 
-const getTags = (plugin) => {
-	return makeRequest(`https://api.github.com/repos/${plugin.author}/${plugin.name}/tags`);
+const sortTags = (tags) => {
+	return tags.sort((tagA, tagB) => semver.gte(tagA.name, tagB.name));
 };
 
-const eachPlugin = async (callback) => {
+const getTags = (plugin, githubToken) => {
+	const options = {};
+	
+	if (githubToken.value) {
+		options.headers = {
+			Authorization: 'Bearer ' + githubToken.value
+		};
+	}
+
+	return makeRequest(`https://api.github.com/repos/${plugin.author}/${plugin.name}/tags`, options);
+};
+
+const eachPlugin = async (githubToken, callback) => {
 	const plugins = (await fetchPlugins())?.plugins;
 
 	await plugins.reduce(async (next, plugin) => {
@@ -49,8 +345,8 @@ const eachPlugin = async (callback) => {
 		};
 
 		try {
-			const packageByTag = await getTagVersion(plugin);
 			const packageByMaster = await getMasterVersion(plugin);
+			const tags = await getTags(plugin, githubToken);
 
 			if (packageByMaster.success) {
 				item.master = packageByMaster.result.version;
@@ -58,16 +354,10 @@ const eachPlugin = async (callback) => {
 				item.masterError = packageByMaster.result.message;
 			}
 
-			if (packageByTag.success) {
-				item.tag = packageByTag.result.version;
+			if (tags.success) {
+				item.tag = sortTags(tags.result)[0].name;
 			} else {
-				const tags = await getTags(plugin);
-
-				if (tags.success) {
-					item.tag = tags.result[0].name;
-				} else {
-					item.tagError = tags.result.message;
-				}
+				item.tagError = tags.result.message;
 			}
 
 			if (item.registry !== item.master) {
@@ -86,27 +376,44 @@ const eachPlugin = async (callback) => {
 	}, Promise.resolve());
 };
 
-const renderTable = (data) => {
-	const table = document.getElementById('plugins-table').querySelector('tbody');
-	table.innerHTML = '';
-	data.forEach((item) => {
-		table.appendChild(createRow(item));
-	});
-};
-
-window.addEventListener('DOMContentLoaded', async () => {
+const renderTable = async (githubToken) => {
 	const spinner = document.querySelector('.spinner-container');
-	try {
-		const result = [];
-
-		await eachPlugin((item) => {
-			result.push(item);
-		});
+	const table = document.getElementById('plugins-table').querySelector('tbody');
 	
-		renderTable(result);
+	spinner.style.display = 'block';
+	table.innerHTML = '';
+
+	try {
+		const data = [];
+
+		await eachPlugin(githubToken, (item) => {
+			data.push(item);
+		});
+
+		table.innerHTML = '';
+		data.forEach((item) => {
+			table.appendChild(createRow(item));
+		});
 	} catch (error) {
 		alert(error.message);
 	} finally {
 		spinner.style.display = 'none';
 	}
+};
+
+window.addEventListener('DOMContentLoaded', async () => {
+	const githubToken = document.getElementById('github-token');
+	const update = document.getElementById('update');
+
+	githubToken.value = localStorage.getItem('github-token');
+	githubToken.addEventListener('change', (e) => {
+		localStorage.setItem('github-token', e.target.value);
+	});
+
+	update.addEventListener('click', async () => {
+		localStorage.setItem('github-token', githubToken.value);
+		await renderTable(githubToken);
+	});
+
+	await renderTable(githubToken);
 });
